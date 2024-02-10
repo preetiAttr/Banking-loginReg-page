dacd6edeb59a2612bd088b20203b5c33fa19c591
Preeti Attri <131139737+preetiAttr@users.noreply.github.com>
2024-02-10 22:38:05 +0530

    Add files via upload
    This the 1st page of banking which has Login & Registration page. I used here HTML, CSS, JavaScript to make this page.

---
+++ script.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector('.login-link');
+const registerlink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close'); // Corrected class name
+
+registerlink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
+loginlink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
